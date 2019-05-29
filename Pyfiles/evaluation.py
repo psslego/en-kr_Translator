50df1b990fd833dadf2f4fd248065f41fce3be83
psslego <psslego@gmail.com>
2019-05-30 01:11:19 +0900

    model accuracy evaluation

---
+++ Pyfiles/evaluation.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch import optim
+import torch.nn.functional as F
+import numpy as np
+
+from model import *
+from preprocess import *
+
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in Okt.morphs(sentence)]
+
+
+def variableFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    result = Variable(torch.LongTensor(indexes).view(-1, 1))
+    if use_cuda:
+        return result.cuda()
+    else:
+        return result
+
+
+def variablesFromPair(input_lang, output_lang, pair):
+    input_variable = variableFromSentence(input_lang, pair[0])
+    target_variable = variableFromSentence(output_lang, pair[1])
+    return (input_variable, target_variable)
+
+
+def evaluate(encoder, decoder, input_lang, sentence, max_length=MAX_LENGTH):
+    input_variable = variableFromSentence(input_lang, sentence)
+    input_length = input_variable.size()[0]
+    encoder_hidden = encoder.initHidden()
+
+    encoder_outputs = Variable(torch.zeros(max_length, encoder.hidden_size))
+    encoder_outputs = encoder_outputs.cuda() if use_cuda else encoder_outputs
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(input_variable[ei],
+                                                 encoder_hidden)
+        encoder_outputs[ei] = encoder_outputs[ei] + encoder_output[0][0]
+
+    decoder_input = Variable(torch.LongTensor([[SOS_token]]))  # SOS
+    decoder_input = decoder_input.cuda() if use_cuda else decoder_input
+
+    decoder_hidden = encoder_hidden
+
+    decoded_words = []
+    decoder_attentions = torch.zeros(max_length, max_length)
+
+    for di in range(max_length):
+        decoder_output, decoder_hidden, decoder_attention = decoder(
+            decoder_input, decoder_hidden, encoder_outputs)
+        decoder_attentions[di] = decoder_attention.data
+        topv, topi = decoder_output.data.topk(1)
+        ni = topi[0][0]
+        if ni == EOS_token:
+            decoded_words.append('<EOS>')
+            break
+        else:
+            decoded_words.append(output_lang.index2word[topi.item()])
+            # decoded_words.append(output_lang.index2word[ni])
+
+        decoder_input = Variable(torch.LongTensor([[ni]]))
+        decoder_input = decoder_input.cuda() if use_cuda else decoder_input
+
+    return decoded_words, decoder_attentions[:di + 1]
+
+
+def evaluateAndShowAttention(encoder1, attn_decoder1, input_lang, input_sentence):
+    output_words, attentions = evaluate(
+        encoder1, attn_decoder1, input_lang, input_sentence)
+    print('output =', ' '.join(output_words))
+
+
+if __name__ == "__main__":
+    encoder = torch.load("./encoder.pd")
+    attn_decoder1 = torch.load("./decoder.pd")
+    input_lang, output_lang, pairs = prepareData()
+    evaluateAndShowAttention(encoder, attn_decoder1, input_lang, "안녕")
